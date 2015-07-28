7d360e79f7ac704a8e51d0ddc1b7bb9e2ffc468a
Guillaume Turri <guillaume.turri@gmail.com>
2015-07-28 19:39:42 +0200

    Improves date parsing
    With this patch, it's now possible to parse those valid dates:
    
    * 2014-11-10T23:38:51+0000
    * 2014-11-10T23:38:51+00:00
    
    Admittedly, the patch is rather messy. The idea comes from
    http://stackoverflow.com/a/2202300/1796345 and the regexes have been used to
    handle a few cases that didn't work out of the box.
    
    I chose this messy code over using Joda time, since an advantage of aXMLRPC,
    is that is has no dependencies
    
    Fix #42

--- src/main/java/de/timroes/axmlrpc/serializer/DateTimeSerializer.java
+++ src/main/java/de/timroes/axmlrpc/serializer/DateTimeSerializer.java
@@ -1,11 +1,14 @@
 package de.timroes.axmlrpc.serializer;
 
+import java.text.SimpleDateFormat;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.w3c.dom.Element;
+
 import de.timroes.axmlrpc.XMLRPCException;
 import de.timroes.axmlrpc.XMLUtil;
 import de.timroes.axmlrpc.xmlcreator.XmlElement;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import org.w3c.dom.Element;
 
 /**
  *
@@ -15,15 +18,34 @@ public class DateTimeSerializer implements Serializer {
 
 	private static final String DATETIME_FORMAT = "yyyyMMdd'T'HH:mm:ss";
 	private static final SimpleDateFormat DATE_FORMATER = new SimpleDateFormat(DATETIME_FORMAT);
+	private static final Pattern PATTERN_WITHOUT_COLON = Pattern.compile("(\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}\\+\\d{2})(\\d{2})");
+	private static final Pattern PATTERN_LEGACY = Pattern.compile("(\\d{4})(\\d{2})(\\d{2}T\\d{2}:\\d{2}:\\d{2})");
 
+	@Override
 	public Object deserialize(Element content) throws XMLRPCException {
 		try {
-			return DATE_FORMATER.parse(XMLUtil.getOnlyTextContent(content.getChildNodes()));
-		} catch (ParseException ex) {
+			String value = formatStringIfNeeded(XMLUtil.getOnlyTextContent(content.getChildNodes()));
+			return javax.xml.bind.DatatypeConverter.parseDateTime(value).getTime();
+		} catch (Exception ex) {
 			throw new XMLRPCException("Unable to parse given date.", ex);
 		}
 	}
 
+	private static String formatStringIfNeeded(String dateStr){
+		Matcher matcherWithoutColon = PATTERN_WITHOUT_COLON.matcher(dateStr);
+		if ( matcherWithoutColon.matches() ){
+			return matcherWithoutColon.group(1) + ":" + matcherWithoutColon.group(2);
+		}
+
+		Matcher matcherLegacy = PATTERN_LEGACY.matcher(dateStr);
+		if ( matcherLegacy.matches() ){
+			return matcherLegacy.group(1) + "-" + matcherLegacy.group(2) + "-" + matcherLegacy.group(3);
+		}
+
+		return dateStr;
+	}
+
+	@Override
 	public XmlElement serialize(Object object) {
 		return XMLUtil.makeXmlTag(SerializerHandler.TYPE_DATETIME,
 				DATE_FORMATER.format(object));
