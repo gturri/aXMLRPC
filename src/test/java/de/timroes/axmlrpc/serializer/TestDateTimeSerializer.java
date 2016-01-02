7451dda31ce490758052bc60a4ee9bd07c9a168e
Guillaume Turri <guillaume.turri@gmail.com>
2016-01-02 19:29:44 +0100

    More rigorous tests on dates
    No more need for approximation due to a bad handling of timezone

--- src/test/java/de/timroes/axmlrpc/serializer/TestDateTimeSerializer.java
+++ src/test/java/de/timroes/axmlrpc/serializer/TestDateTimeSerializer.java
@@ -3,31 +3,39 @@ package de.timroes.axmlrpc.serializer;
 import static org.junit.Assert.*;
 
 import java.util.Date;
+import java.util.TimeZone;
 
 public class TestDateTimeSerializer {
-	private static final Date EXPECTED_DATE = new Date(85, 2, 4, 12, 13, 14);
+	private Date expectedDate = new Date(85, 2, 4, 12, 13, 14);
+	private TimeZone _previousTZ;
+
+	@org.junit.Before
+	public void setUp(){
+		_previousTZ = TimeZone.getDefault();
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+		expectedDate = new Date(85, 2, 4, 12, 13, 14);
+	}
+
+	@org.junit.After
+	public void tearDown(){
+		TimeZone.setDefault(_previousTZ);
+	}
 
 	@org.junit.Test
 	public void canParseLegacyDates() throws Exception {
 		Date date = (Date) new DateTimeSerializer().deserialize("19850304T12:13:14");
-		assertDatesCloseEnough(EXPECTED_DATE, date);
+		assertEquals(expectedDate, date);
 	}
 
 	@org.junit.Test
 	public void canParseDateWithoutSemiColon() throws Exception {
-		Date date = (Date) new DateTimeSerializer().deserialize("1985-03-04T12:13:14+0100");
-		assertDatesCloseEnough(EXPECTED_DATE, date);
+		Date date = (Date) new DateTimeSerializer().deserialize("1985-03-04T13:13:14+0100");
+		assertEquals(expectedDate, date);
 	}
 
 	@org.junit.Test
 	public void canParseDateWithSemiColon() throws Exception {
-		Date date = (Date) new DateTimeSerializer().deserialize("1985-03-04T12:13:14+01:00");
-		assertDatesCloseEnough(EXPECTED_DATE, date);
-	}
-
-	//Because I don't want the tests to fail if the user isn't in my timezone
-	private void assertDatesCloseEnough(Date expected, Date actual){
-		long differenceInMs = Math.abs(expected.getTime() - actual.getTime());
-		assertTrue(differenceInMs < 24 * 60 * 60 * 1000);
+		Date date = (Date) new DateTimeSerializer().deserialize("1985-03-04T13:13:14+01:00");
+		assertEquals(expectedDate, date);
 	}
 }
